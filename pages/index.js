ceb1815c76591cfffd923cf5fef30a318ea086a8
Bruno Bulnes <bulnes.bruno@gmail.com>
2021-11-20 13:55:12 -0300

    task: structure colors helper

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,59 @@
 import Head from 'next/head'
+import coresProvas from '../helpers/cores-provas'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="block min-h-screen">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="description" content="Acompanhe o gabarito oficial e a correção de todas as questões do Enem 2021 no R7 Educação." />
+        <meta name="author" content="https://www.r7.com" />
+        <meta name="publisher" content="https://www.r7.com" />
+
+        <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/favicon.png" />
+
+        <title>Correção Enem 2021 - Notícias - R7 Educação</title>
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      {/* <div className="flex items-center justify-center bg-gray-100 w-screen h-screen">
+        <div 
+          style={{ borderTopColor: 'transparent' }} 
+          className="w-16 h-16 border-4 border-blue-400 border-solid rounded-full animate-spin">
         </div>
-      </main>
+      </div> */}
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <main className="container mx-auto my-5 px-8 md:py-0">
+        <header className="grid grid-cols-1 grid-rows-2 gap-5 lg:grid-cols-2 lg:grid-rows-1">
+          {/* Dia da Prova */}
+          <div>
+            <label htmlFor="provaDia" className="block text-gray-700 font-bold mb-2">
+              Selecione o dia da sua prova
+            </label>
+
+            <select name="provaDia" id="provaDia" className="shadow border bg-white rounded w-full py-2 px-3 leading-tight max-w-md">
+              <option value="01">Primeiro dia</option>
+              <option value="02">Segundo dia</option>
+            </select>
+          </div>
+
+          {/* Cores das Provas */}
+          <div className="max-w-3xl">
+            <div className="block text-gray-700 font-bold mb-2">
+              Selecione o dia da sua prova
+            </div>
+
+            <div className="grid grid-cols-3 grid-rows-2 gap-2">
+              {coresProvas.map((cp, i) => (
+                <label className="grid grid-cols-3 gap-1 items-center w-20" key={i}>
+                  <input type="radio" name="corProva" value={cp.cor} defaultChecked={cp.checked} />
+                  <span className={`block h-5 w-5 border ${cp.cor}`}></span>
+                  <span className="capitalize">{cp.nome}</span>
+                </label>
+              ))}
+            </div>
+          </div>
+        </header>
+      </main>
     </div>
   )
 }
