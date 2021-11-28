b0c1689f03ceaba69545a6f7840efb0faedd74fc
Bruno Bulnes <bulnes.bruno@gmail.com>
2021-11-28 18:37:57 -0300

    task: change link

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer>
       <p className="text-center">
-        <a href="http://site.estudenoanglo.com.br/correcao_online/correcao_online.php?codcorrecao=118" title="Clique aqui para ver a resolução comentada do Enem 2021" target="_blank" className="text-blue-700 pb-px border-b border-blue-700">Clique aqui</a> e veja a resolução comentada das questões e da redação feita pelos professores do Anglo Leonardo da Vinci
+        <a href="http://site.estudenoanglo.com.br/correcao_online/correcao_online.php?codcorrecao=119" title="Clique aqui para ver a resolução comentada do Enem 2021" target="_blank" className="text-blue-700 pb-px border-b border-blue-700">Clique aqui</a> e veja a resolução comentada das questões e da redação feita pelos professores do Anglo Leonardo da Vinci
       </p>
     </footer>
   )
